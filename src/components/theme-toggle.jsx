9f10750de360f5928d83462d7da7dd71f71bcb7d
Alex Perronnet <aperronnet.login@gmail.com>
2022-08-11 22:05:39 +0200

    :sparkles: Add theme switcher

--- src/components/theme-toggle.jsx
+++ src/components/theme-toggle.jsx
@@ -1,7 +1,24 @@
 import styled from "styled-components"
+import { ReactComponent as Sun } from "~/assets/images/sun.svg"
+import { ReactComponent as Moon } from "~/assets/images/moon.svg"
 
-export const ThemeToggle = () => {
-  return <StyledThemeToggle></StyledThemeToggle>
+export const ThemeToggle = ({ onClick, theme }) => {
+  return (
+    <StyledThemeToggle onClick={onClick}>
+      {theme === "light" ? <Sun /> : <Moon />}
+    </StyledThemeToggle>
+  )
 }
 
-const StyledThemeToggle = styled.button``
+const StyledThemeToggle = styled.button`
+  background-color: transparent;
+  color: ${({ theme }) => theme.foreground};
+  border: none;
+  line-height: 0;
+  cursor: pointer;
+  transition: opacity 150ms linear;
+
+  &:hover {
+    opacity: 0.7;
+  }
+`
--- src/hooks/use-theme.jsx
+++ src/hooks/use-theme.jsx
@@ -1,1 +1,30 @@
-export const useThemes = () => {}
+import { useEffect, useState } from "react"
+
+export const useThemes = () => {
+  const [theme, setTheme] = useState("light")
+
+  const setMode = mode => {
+    window.localStorage.setItem("theme", mode)
+    setTheme(mode)
+  }
+
+  const handleToggle = () => {
+    if (theme === "light") {
+      setMode("dark")
+    } else {
+      setMode("light")
+    }
+  }
+
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem("theme")
+
+    if (localTheme) {
+      setTheme(localTheme)
+    } else {
+      setMode("light")
+    }
+  }, [])
+
+  return [theme, handleToggle]
+}
