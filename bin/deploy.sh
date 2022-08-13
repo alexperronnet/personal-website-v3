2240c4515cb294629562e89809c20e097cd72d62
Alex Perronnet <aperronnet.login@gmail.com>
2022-08-14 01:35:21 +0200

    Update deployment script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -13,6 +13,6 @@ git init
 git checkout -b main
 git add -A
 git commit -m 'Deploy'
-git push -f git@github.com:alexperronnet/personal-website-v3.git main:gh-pages
+git push -f git@github.com:alexperronnet/personal-website-v3.git main:production
 
 cd -
