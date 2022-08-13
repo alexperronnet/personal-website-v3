a8104276c34d8c0ad2921c641be4675dec3725b0
Alex Perronnet <aperronnet.login@gmail.com>
2022-08-14 01:24:29 +0200

    Add deployment script

---
+++ bin/deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'Deploy'
+git push -f git@github.com:alexperronnet/personal-website-v3.git main:gh-pages
+
+cd -
